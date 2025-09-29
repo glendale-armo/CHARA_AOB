6b2eaa9bf170663a3dd95a3701277263bd7832c1
noy-hakobyan <hakobyannoy812@gmail.com>
2025-09-29 01:32:03 -0700

    Update config.h

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 #define CONFIG_H
 
 #include <Arduino.h>
+#include <Ethernet.h>
 
 // Network
 #define PORT_NUM            8888
@@ -9,8 +10,14 @@
 static inline IPAddress deviceIP() { return IPAddress(192, 168, 0, 123); }
 
 // RS-485 / Modbus
+// COM-1 remains the primary. COM-0 is added transparently in driver_io.h.
 #define SerialPort          Serial1
 #define MODBUS_BAUD         19200UL
+
+// Secondary RS-485 bus enable (COM-0). Set to 0 to disable COM-0.
+#define USE_COM0            1
+#define SerialPortA         Serial1   // COM-1
+#define SerialPortB         Serial0   // COM-0
 
 // PR0 motion presets
 #define RPM                 50u           // PR0 velocity (RPM)
